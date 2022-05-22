e8c10b5171bfc8356508421a8be3ead2ec419f9d
TOWHIDpro <sajjad.rk05@gmail.com>
2022-05-22 17:45:53 +0600

    final2

--- testimg.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image 
-import PIL 
-  
-# creating a image object (main image) 
-im1 = Image.open(r"D:\\New folder\\image_merge\\uploadimg\\static\\images\\shirt.jpg") 
-  
-# save a image using extension
-im1.save("logo.jpg")
